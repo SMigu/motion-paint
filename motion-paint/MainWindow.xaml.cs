58fa6467594ba3b68d4653ea7d89e05baf21bf79
Mika Lamminaho <mika.k.lamminaho@gmail.com>
2014-03-16 22:17:19 +0200

    Some work on the kinect interaction
    - kinect interaction stream event
    - hand activity tracking

--- motion-paint/MainWindow.xaml.cs
+++ motion-paint/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Interaction;
+using Microsoft.Kinect.Toolkit.Controls;
+
 
 namespace motion_paint
 {
@@ -23,6 +27,12 @@ namespace motion_paint
     public partial class MainWindow : Window
     {
         private KinectSensorChooser sensorChooser;
+
+        private InteractionStream _interactionStream;
+        private Skeleton[] _skeletons; //the skeletons 
+        private UserInfo[] _userInfos; //the information about the interactive users
+        private KinectSensor _sensor;
+       
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +41,9 @@ namespace motion_paint
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
+            if (DesignerProperties.GetIsInDesignMode(this))
+                return;
+
             this.sensorChooser = new KinectSensorChooser();
             this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
             this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
@@ -60,21 +73,40 @@ namespace motion_paint
             {
                 try
                 {
-                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                    args.NewSensor.SkeletonStream.Enable();
+                    var parameters = new TransformSmoothParameters
+                    {
+                        Smoothing = 0.3f,
+                        Correction = 0.0f,
+                        Prediction = 0.0f,
+                        JitterRadius = 1.0f,
+                        MaxDeviationRadius = 0.5f
+                    };
+
+                    _sensor = args.NewSensor;
+
+                    _sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    _sensor.SkeletonStream.Enable(parameters);
+                    _skeletons = new Skeleton[_sensor.SkeletonStream.FrameSkeletonArrayLength];
+                    _userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
 
                     try 
 	                {	        
-		                args.NewSensor.DepthStream.Range = DepthRange.Near;
-                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
-                        args.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+		                _sensor.DepthStream.Range = DepthRange.Near;
+                        _sensor.SkeletonStream.EnableTrackingInNearRange = true;
+                        _sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
 	                }
 	                catch (InvalidOperationException)
 	                {
-		                args.NewSensor.DepthStream.Range = DepthRange.Default;
-                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+		                _sensor.DepthStream.Range = DepthRange.Default;
+                        _sensor.SkeletonStream.EnableTrackingInNearRange = false;
 		                error = true;
 	                }
+
+                    _interactionStream = new InteractionStream(_sensor, new DummyInteractionClient());
+                    _interactionStream.InteractionFrameReady += InteractionStreamOnInteractionFrameReady;
+
+                    _sensor.DepthFrameReady += SensorOnDepthFrameReady;
+                    _sensor.SkeletonFrameReady += SensorOnSkeletonFrameReady;
                 }
                 catch(InvalidOperationException)
                 {
@@ -82,6 +114,121 @@ namespace motion_paint
                 }
             }
 
+            if (!error)
+            {
+                _sensor.Start();
+                kinectRegion.KinectSensor = _sensor;
+            }
+        }
+
+        private void SensorOnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs skeletonFrameReadyEventArgs)
+        {
+            using (SkeletonFrame skeletonFrame = skeletonFrameReadyEventArgs.OpenSkeletonFrame())
+            {
+                if (skeletonFrame == null)
+                {
+                    return;
+                }
+
+                try
+                {
+                    skeletonFrame.CopySkeletonDataTo(_skeletons);
+                    var accelerometerReading = _sensor.AccelerometerGetCurrentReading();
+                    _interactionStream.ProcessSkeleton(_skeletons, accelerometerReading, skeletonFrame.Timestamp);
+                }
+                catch (InvalidOperationException)
+                {
+                    // If exception skip frame
+                }
+            }
+        }
+
+        private void SensorOnDepthFrameReady(object sender, DepthImageFrameReadyEventArgs depthImageFrameReadyEventArgs)
+        {
+            using (DepthImageFrame depthFrame = depthImageFrameReadyEventArgs.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                {
+                    return;
+                }
+
+                try
+                {
+                    _interactionStream.ProcessDepth(depthFrame.GetRawPixelData(), depthFrame.Timestamp);
+                }
+                catch (InvalidOperationException)
+                {
+                    // If exception skip frame
+                }
+            }
+        }
+
+        private Dictionary<int, InteractionHandEventType> _lastLeftHandEvents = new Dictionary<int, InteractionHandEventType>();
+        private Dictionary<int, InteractionHandEventType> _lastRightHandEvents = new Dictionary<int, InteractionHandEventType>();
+        private Dictionary<int, bool> _lastActiveLeftHands = new Dictionary<int, bool>();
+        private Dictionary<int, bool> _lastActiveRightHands = new Dictionary<int, bool>();
+        
+        // Id of the primary user. 
+        // TODO Make better chooser 
+        int primaryUserId = 1;
+        public Vector _primaryPointerPosition;
+
+        private void InteractionStreamOnInteractionFrameReady(object sender, InteractionFrameReadyEventArgs e)
+        {
+            using (var iaf = e.OpenInteractionFrame())
+            {
+                if (iaf == null)
+                    return;
+
+                iaf.CopyInteractionDataTo(_userInfos);
+            }
+
+            var hasUser = false;
+            foreach (var userInfo in _userInfos)
+            {
+                var userID = userInfo.SkeletonTrackingId;
+                if (userID == 0)
+                {
+                    continue;
+                }
+                hasUser = true;
+                var hands = userInfo.HandPointers;
+                if (hands.Count == 0)
+                {
+                }
+                else
+                {
+                    foreach (var hand in hands)
+                    {
+                        var lastHandEvents = hand.HandType == InteractionHandType.Left ? _lastLeftHandEvents : _lastRightHandEvents;
+                        var lastActiveHands = hand.HandType == InteractionHandType.Left ? _lastActiveLeftHands : _lastActiveRightHands;
+                        
+                        if (hand.HandEventType != InteractionHandEventType.None)
+                        {
+                            lastHandEvents[userID] = hand.HandEventType;
+                        }
+                        lastActiveHands[userID] = hand.IsActive;
+                        
+                        var screenX = hp.X * kinectRegion.ActualWidth;
+                        var screenY = hp.Y * kinectRegion.ActualHeight;
+                        _primaryPointerPosition = new Vector(screenX, screenY);
+                    }
+                }
+            }
+
+            // for activating and disabling draw (two hand draw mode)
+            if (_lastActiveLeftHands[primaryUserId] && _lastActiveRightHands[primaryUserId])
+            {
+                // if both hands are active call startDraw()
+            }
+            else
+            {
+                // if one or both hands are inactive call disableDraw()
+            }
+
+            if (!hasUser)
+            {
+            }
         }
     }
 }
